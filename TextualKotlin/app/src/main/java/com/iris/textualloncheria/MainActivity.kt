5d7f375f5686c7d56a28484ea9efb28ee298f9ac
Eduardo Yair Cuellar Rivera <eycr.nm@gmail.com>
2021-03-08 17:00:49 -0600

    Cambio de paleta de colores a la establecida

--- TextualKotlin/app/src/main/java/com/iris/textualloncheria/MainActivity.kt
+++ TextualKotlin/app/src/main/java/com/iris/textualloncheria/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
